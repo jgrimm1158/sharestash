25a3d75e0e5cdfad69530a1e740116197604b91b
jgrimm1158 <jonathangrimm1158@gmail.com>
2014-01-15 13:54:15 -0600

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sharestash::Application.config.secret_key_base = 'ce3ea726ce9188e689029d08bd6db2c9f6b4aa982a9a5a7e48b2bb13c0c7a077375270b6939c47cff01a2808966970a7915c2dc820409a94d5759dcc5cca1d32'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sharestash::Application.config.session_store :cookie_store, key: '_sharestash_session'
